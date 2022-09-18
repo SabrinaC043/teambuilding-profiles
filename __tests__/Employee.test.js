5cc8cf7eafc55e18ea4142ffe070af1a5e02beb9
Sabrina Carrion <SabrinaCarrion43@gmail.com>
2022-09-18 16:01:09 -0500

    testing practice

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,23 @@
+const Employee = require('../lib/Employee');
+
+const emp = new Employee('sabrina', 2, 'test@test.com');
+
+describe('name', () => {
+    it('Should return Employee name', () => {
+        expect(emp.name).toBe('sabrina')
+    })
+
+});
+
+describe('id', () => {
+    it('Should return id number', () => {
+        expect(emp.id).toBe(2)
+    })
+
+});
+
+
+describe('email', () => {
+    it('Should return email address', () => {
+        expect(emp.email).toBe('test@test.com')
+    });
---
+++ index.js
@@ -0,0 +1,16 @@
+// Include Packages for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// const generateHTML = require('./')
+
+
+const teamMembers = []
+
+
+
+function generateHTMLFile(userResponses) {
+    // place HTML here 
+
+
+}
