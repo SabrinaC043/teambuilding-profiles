92cf75a567cfad5e1949dc4b503baccabebb0ea1
Sabrina Carrion <SabrinaCarrion43@gmail.com>
2022-09-20 09:59:20 -0500

    questions not showing up one after the other for all employees

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 // Include Packages for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Employee = require('./lib/Employee');
-const Employee = new Employee();
+
 
 inquirer
   .prompt([{
 
     type: 'input',
     name: 'names',
-    message: 'What is your name?',
-    default: "enter full name",
+    message: 'What is the Employees name?',
+    // default: "enter full name",
   },
   {
     type: 'input',
     name: 'email',
     message: 'What is your email address?',
-    default: "Please enter a valid email address",
+    // default: "Please enter a valid email address",
   },
   {
     type: 'input',
@@ -40,28 +39,35 @@ inquirer
     type: 'input',
     name: 'school',
     message: 'What is the name of your school?',
-    default: "Please enter a school name",
+    // default: "Please enter a school name",
   },
 
 
   ]);
 
 // function to write index.html file 
-function writetoFile(fileName, data) {
-  return fs.writeFileSync(fileName, data)
-}
+
+fs.writeFile('index.html', data, (err) => {
+  console.log("Error:", err)
+})
+fs.appendFile('index.html', data, {
+  encoding: 'utf8'
+}, (err) => {
+  console.log("Error:", err)
+})
+
 
 // create a function to kick off 
-function create() {
+// function create() {
 
-  inquirer.prompt(questions).then((data) => {
-    writetoFile('index.html', generateHTMLFile(data))
-  }).catch((error) => {
-    console.log(error);
-  });
-}
+//   inquirer.prompt(questions).then((data) => {
+//     writetoFile('index.html', generateHTMLFile(data))
+//   }).catch((error) => {
+//     console.log(error);
+//   });
+// }
 
-function generateHTMLFile() {
-};
+// function generateHTMLFile() {
+// };
 
-create()
+// create()
