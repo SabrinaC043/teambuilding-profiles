daa4c7877f00f84d764ea2511177056271d93c5f
Sabrina Carrion <SabrinaCarrion43@gmail.com>
2022-09-24 19:50:27 -0500

    testing video

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -14,16 +14,17 @@ function generateTeam(team) {
   function generateEmployeeCard(employee) {
     return `
 <div class="card" style="width: 18rem">
-  <div>
-    <h5 class="card-title">Employee:${employee.name}</h5>
-      <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-badge-fill" viewBox="0 0 16 16">
-        <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm4.5 0a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm5 2.755C12.146 12.825 10.623 12 8 12s-4.146.826-5 1.755V14a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-.245z"/>
-      </svg>
+  <div class="card-title">
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-badge-fill" viewBox="0 0 16 16">
+      <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm4.5 0a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm5 2.755C12.146 12.825 10.623 12 8 12s-4.146.826-5 1.755V14a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-.245z"/>
+    </svg>Employee:${employee.name}
+      
   </div>
   <ul class="list-group list-group-flush">
    <li class="list-group-item">ID: ${employee.id}</li>
   <div class="card-body" style= "background-color: #B3C5D7"> 
-    <div class="card-text">Email:<a href="${employee.email}" class="card-link"></a>
+    <div class="card-text">Email:<a href="https://${employee.email}" class="card-link">${employee.email}</a>
+    </div>
   </div>
  
   ${employee.getRole() === "Manager" ? `
